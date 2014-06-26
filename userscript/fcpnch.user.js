76ebb110d6a5d6bec8eaa6a63868580c3f48c579
LennyPenny <l.bernhardt@live.de>
2014-06-26 20:28:52 +0200

    update usrscrpt to even shorta

--- userscript/fcpnch.user.js
+++ userscript/fcpnch.user.js
@@ -12,13 +12,6 @@
 var myCss = GM_getResourceText("fpnchcss");
 GM_addStyle(myCss);
 
-function getThreadID() {
-	var re = /t=\w+/g;
-
-	return $(location).attr("href").match(re)[0].slice(2)
-};
-var threadID = Number(getThreadID());
-
 var lookUp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 function base62(n, b) {
 	var res = []
@@ -29,8 +22,6 @@ function base62(n, b) {
 	} while (n > 0);
 	return res.join("");
 }
-
-var shortThreadID = base62(threadID, 62);
 
 var shortner = document.createElement("a");
 shortner.innerHTML = "shortn";
@@ -44,6 +35,6 @@ $(".fcpnchBtn").click(function(event) {
 
 	var shortPostID = base62(postID, 62);
 
-	var toCopy = "http://fcpn.ch/" + shortThreadID + "/" + shortPostID;
+	var toCopy = "http://fcpn.ch/" + shortPostID;
 	GM_setClipboard(toCopy);
 });
