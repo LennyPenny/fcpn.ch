23167feff75bcd4c66fcfd0efcb8d05d62de444e
LennyPenny <l.bernhardt@live.de>
2014-06-26 20:26:09 +0200

    hotfix

--- fcpn.ch/redirecter.lua
+++ fcpn.ch/redirecter.lua
@@ -22,4 +22,4 @@ if #uri == 0 then ngx.redirect("http://shortn.fcpn.ch") return end
 local postID, err = to10(string.match(uri, "%w+"), 62)
 if not postID then ngx.say("Invalid char: "..err) return end
 
-ngx.redirect("http://facepunch.com/showthread.php?p="..parts[1].."#post"..parts[1])
+ngx.redirect("http://facepunch.com/showthread.php?p="..postID.."#post"..postID)
