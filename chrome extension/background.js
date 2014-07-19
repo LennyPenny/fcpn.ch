70f11f9bcf29ff3f8cda2ba3423522e3bfbd01e4
Nicholas Anderson <nander13@gmu.edu>
2014-07-19 18:10:27 -0400

    Redirect without hitting server
    Utilizes onBeforeRequest to intercept the
    requests to fcpn.ch, and redirect without actually
    needed to touch the server.

--- chrome extension/background.js
+++ chrome extension/background.js
@@ -17,4 +17,24 @@ function callBack(msg, sender, sendResponse) {
 	};
 };
 
+var lookUp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+function base10(str) {
+	for (decodedNum = 0, i = len = str.length; i--;) {
+		decodedNum += lookUp.indexOf(str[i]) * Math.pow(62, len - i - 1);
+	}
+	return decodedNum;
+}
+
 chrome.runtime.onMessage.addListener(callBack);
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function(newPage) {
+		postId = base10(newPage.url.match(/^https?:\/\/fcpn\.ch\/(.*)$/)[1]);
+		console.log("Redirecting to: %s", postId);
+		return { redirectUrl: "http://www.facepunch.com/showthread.php?p="+postId+"#post"+postId }
+	}, {
+		urls: ["*://fcpn.ch/*"]
+	}, [
+		"blocking"
+	]
+) 
